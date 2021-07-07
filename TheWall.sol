97d1267ca629b6ca3164b186b81afe163d280361
patriotswap <86752925+patriotswap@users.noreply.github.com>
2021-07-06 22:39:43 -0400

    Update TheWall.sol

--- TheWall.sol
+++ TheWall.sol
@@ -1,8 +1,6 @@
 pragma solidity 0.6.12;
 
 
-//removed public BASE_URI getter function from underlying ERC721 as it wasn't necessary
-
 import "@openzeppelin/contracts/presets/ERC721PresetMinterPauserAutoId.sol";
 
 contract TheWall is ERC721PresetMinterPauserAutoId("THE WALL","THEWALL",<REDACTED>) {
